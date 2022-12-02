12f324780bc3859503ba563fc025e630597cee66
Narsiles <92451486+Narsiles@users.noreply.github.com>
2022-12-02 12:01:18 +0100

    UI

--- Assets/MyGame/Scripts/Menu.cs
+++ Assets/MyGame/Scripts/Menu.cs
@@ -1,11 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class Menu : MonoBehaviour
 {
-    public void OnClickButton(int levelId)
+
+    // Start is called before the first frame update
+    void Start()
     {
-        Debug.Log("OnClickButton");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ToGame()
+    {
+        SceneManager.LoadScene("Feur");
     }
 }
